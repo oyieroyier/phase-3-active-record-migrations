61076c76c77dfe9a6e7483e89e6d779def2932a5
Bob Oyier <boboyier92@gmail.com>
2023-03-01 00:01:03 +0300

    Completed assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
-ENV["RACK_ENV"] ||= "development"
+ENV['RACK_ENV'] ||= 'development'
 
 require 'bundler/setup'
-Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require(:default, ENV['RACK_ENV'])
 
-require_relative "../app/models/artist"
+require_relative '../app/models/artist'
